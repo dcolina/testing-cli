320a576a96a9f3a64a67764fca8d740198326a37
daniel.colina <daniel.colina@dotcms.com>
2023-11-28 19:43:25 +0100

    Testing GHA push command with the lastest changes. +8

--- .github/workflows/scripts/run-push.sh
+++ .github/workflows/scripts/run-push.sh
@@ -98,7 +98,7 @@ _run_cli_push(){
       export QUARKUS_LOG_FILE_PATH="$DOT_CLI_HOME"dotcms-cli.log
 
       if [[ -z $push_opts ]]; then
-        "$(bash /tmp/dot-cli/run-java.sh push "$workspace_path" --token="$token")"
+        "$(bash /tmp/dot-cli/run-java.sh push "$workspace_path")"
         echo "No push options provided"
       else
         /tmp/dot-cli/run-java.sh push "$workspace_path" $push_opts --token="$token"
